7fb99eb25e5aac946956ceae1f7eef8a509ccb7d
Timur Demenov <stroggif38@gmail.com>
2022-03-11 04:21:24 +0600

    feat: add basic page & init opus-media-recorder

--- Project/frontend-pwa/src/App.tsx
+++ Project/frontend-pwa/src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 
-function App() {
+import OpusMediaRecorderView from "./utils/OpusMediaRecorderView";
+import "./App.css";
+
+const App: FC = () => {
+  const data: any = [];
+
+  const [state, setState] = useState({ data: data, blob: new Blob(data) });
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OpusMediaRecorderView
+        onDataAvailable={async (e: any) => {
+          setLoading(true);
+          const data = [...state.data, e.data];
+          setState({
+            data: data,
+            blob: new Blob(data, { type: "audio/ogg; codecs=opus" }),
+          });
+
+          const formData = new FormData();
+          formData.append("file", data[0]);
+
+          const res = await fetch("http://192.168.0.104:8000/stt_sync", {
+            method: "POST",
+            body: formData,
+          });
+          const json = await res.json();
+          console.log(json);
+          setResult(json.result);
+          setLoading(false);
+        }}
+        render={({
+          state,
+          start,
+          stop,
+          pause,
+          resume,
+        }: {
+          state: any;
+          start: any;
+          stop: any;
+          pause: any;
+          resume: any;
+        }) => (
+          <Card sx={{ maxWidth: 500, height: 450 }}>
+            <StyledCardContent>
+              <audio controls />
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {state}
+              </Typography>
+              <Button onClick={start}>Start Recording</Button>
+              <Button onClick={stop}>Stop Recording</Button>
+              <Typography>{result}</Typography>
+              {loading && <CircularProgress />}
+            </StyledCardContent>
+          </Card>
+        )}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+const StyledCardContent = styled(CardContent)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
