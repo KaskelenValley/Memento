0b24c3e67461d86fff2012ae5522811130e55971
Madiyar Mukushev <m.mukushev@visionlabs.ru>
2022-03-10 23:28:10 +0600

    feat: minor changes

--- Project/ml-backend/main.py
+++ Project/ml-backend/main.py
@@ -40,8 +40,8 @@ async def convert_stt_sync(file: UploadFile):
 
     data = await file.read()
     file_ext = magic.from_buffer(data, mime=True)
-    if file_ext != 'audio/ogg':
-        return {"message": f"Only .ogg audio type supported!. Provided file's extension is {file_ext}"}
+    # if file_ext != 'audio/ogg':
+    #     return {"message": f"Only .ogg audio type supported!. Provided file's extension is {file_ext}"}
 
     url = urllib.request.Request("https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?%s" % params, data=data)
     url.add_header("Authorization", "Bearer %s" % IAM_TOKEN)
