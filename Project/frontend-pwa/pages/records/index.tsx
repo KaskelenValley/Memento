066ecd872c949a6435be5e1ecae6c0d0d6ffca5e
Timur Demenov <stroggif38@gmail.com>
2022-04-26 18:23:56 +0600

    chore: update records page

--- Project/frontend-pwa/pages/records/index.tsx
+++ Project/frontend-pwa/pages/records/index.tsx
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Button,
   CircularProgress,
   Container,
   css,
@@ -14,20 +10,20 @@ import {
 import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { deleteObject, getBlob, ref } from "firebase/storage";
 import { useAuthState } from "react-firebase-hooks/auth";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 import { auth, db, storage } from "../../utils/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { ArrowIcon } from "../../icons";
+import { RecordWaveIcon } from "../../icons";
 import CloseButton from "../../components/Button/CloseButton";
+import { RecordCard } from "../../components/RecordCard";
+import { groupByDate } from "../../utils";
 
 const Records = (props) => {
   const [records, setRecords] = useState<any>();
   const [filtered, setFiltered] = useState<any>();
   const [text, setText] = useState<any>([]);
 
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const { push } = useRouter();
 
   let docRef;
@@ -52,7 +48,7 @@ const Records = (props) => {
           }
 
           setRecords(arr);
-          setFiltered(arr);
+          setFiltered(groupByDate(arr));
           setText(arr);
         }
       );
@@ -63,7 +59,9 @@ const Records = (props) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFiltered(
-      records.filter((e) => e.result.toLowerCase().includes(event.target.value))
+      records.filter((e) =>
+        (e.result + e.title).toLowerCase().includes(event.target.value)
+      )
     );
   };
 
@@ -104,6 +102,7 @@ const Records = (props) => {
     });
     alert("Updated!");
   };
+  console.log(records);
 
   return (
     <StyledContainer>
@@ -128,30 +127,89 @@ const Records = (props) => {
         sx={{ width: "100%" }}
       />
       <RecordsContainer>
-        {filtered && !filtered.length && (
+        {filtered && Object.keys(filtered).length === 0 && (
           <Typography>There is no records</Typography>
         )}
         {filtered ? (
-          filtered.map((e, i) => {
-            return (
-              <StyledAccordion key={i}>
-                <AccordionSummary>
-                  <Typography>{e.result}</Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                  style={{ display: "flex", flexDirection: "column" }}
+          Object.entries(filtered).map(([date, arr]) => (
+            <>
+              <DateContainer>
+                <Typography
+                  sx={{
+                    fontFamily: "Georgia",
+                    fontWeight: 700,
+                    fontSize: 13,
+                  }}
                 >
-                  <Button onClick={() => deleteRecord(e.id)}>
-                    Delete record
-                  </Button>
-                  <Button onClick={() => push(`/records/${e.id}`)}>Open</Button>
-                </AccordionDetails>
-              </StyledAccordion>
+                  {`${new Date(date).toLocaleString("en-US", {
+                    weekday: "long",
+                  })}/${new Date(date).toLocaleString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                  })}`}
+                </Typography>
+              </DateContainer>
+              {arr.map((record, i) => {
+                return (
+                  <RecordContainer key={i}>
+                    <CardContainer>
+                      <RecordWaveIcon />
+                      <Typography
+                        sx={{
+                          fontWeight: 500,
+                          fontSize: "13px",
+                          color: "#69696A",
+                          ml: 1,
+                        }}
+                      >
+                        {record.date.toDate().toLocaleTimeString("en-US", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </Typography>
+                    </CardContainer>
+                    <CardWrapper>
+                      <StyledHr />
+                      <RecordCard record={record} />
+                    </CardWrapper>
+                  </RecordContainer>
+                );
+              })}
+            </>
+          ))
+        ) : (
+          <CircularProgress />
+        )}
+        {/* {filtered ? (
+          filtered.map((record, i) => {
+            return (
+              <RecordContainer key={i}>
+                <CardContainer>
+                  <RecordWaveIcon />
+                  <Typography
+                    sx={{
+                      fontWeight: 500,
+                      fontSize: "13px",
+                      color: "#69696A",
+                      ml: 1,
+                    }}
+                  >
+                    {record.date.toDate().toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </Typography>
+                </CardContainer>
+                <CardWrapper>
+                  <StyledHr />
+                  <RecordCard record={record} />
+                </CardWrapper>
+              </RecordContainer>
             );
           })
         ) : (
           <CircularProgress />
-        )}
+        )} */}
       </RecordsContainer>
     </StyledContainer>
   );
@@ -181,31 +239,12 @@ const RecordsContainer = styled("div")`
   `}
 `;
 
-const StyledAccordion = styled(Accordion)`
+const RecordContainer = styled("div")`
   ${({ theme }) => css`
-    background: #f8f8f8;
-    border-radius: 34px;
-    margin-bottom: 8px;
-    box-shadow: none;
     width: 100%;
-
-    audio {
-      width: 100%;
-    }
-
-    p {
-      font-size: 14px;
-      font-weight: 400;
-    }
-
-    &:first-of-type,
-    &:last-of-type {
-      border-radius: 34px;
-    }
-
-    &::before {
-      content: none;
-    }
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
   `}
 `;
 
@@ -219,5 +258,32 @@ const StyledBlock = styled("div")`
   & > svg {
     position: absolute;
     left: 0;
+  }
+`;
+
+const CardContainer = styled("div")`
+  display: flex;
+  align-items: center;
+`;
+
+const DateContainer = styled("div")`
+  border: 1px solid #69696a;
+  padding: 5px 8px;
+  border-radius: 12px;
+  margin: 30px 0;
+`;
+
+const StyledHr = styled("hr")`
+  margin: 0 21.5px 0 14.5px;
+  border: 1px solid rgba(44, 44, 44, 0.25);
+`;
+
+const CardWrapper = styled("div")`
+  display: flex;
+  justify-content: center;
+  margin-top: 6px;
+
+  & > div {
+    margin: 7.5px 0;
   }
 `;
